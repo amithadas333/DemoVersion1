30c7c3f4883b1cbe4b7c3c0fb2be6aa3fb8efc2f
dasam <dasam@LAPTOP-94M9N0R5>
2023-08-08 15:42:49 +0530

    newly added first commit

---
+++ src/demopurpose/hello.java
@@ -0,0 +1,17 @@
+package demopurpose;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "amitdha";
+		if (name == "amitha") {
+			System.out.println("print happy");
+		} else {
+			System.out.println("not happy");
+		}
+
+	}
+
+}
